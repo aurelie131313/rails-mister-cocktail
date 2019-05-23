19ba82ca88b70e1a0b08164b9fed2f84458609ac
Aurelie GIMET <aurelie.gimet@kedgebs.com>
2019-05-23 17:50:02 +0200

    moche mais passe

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,7 @@
 class Ingredient < ApplicationRecord
-  validates :name, uniqueness: true, presence: true
 
   has_many :doses
+  has_many :cocktails, through: :doses
+  validates :name, uniqueness: true, presence: true
+
 end
